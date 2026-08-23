64fe27bd83aa2e61bce5b500d19373d06376c84a
petyo.kirilov <petiofiiito@gmail.com>
2026-08-23 03:21:09 +0300

    add event custom plugin

---
+++ src/plugins/events/events.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name:       Events
+ * Description:       Custom Event post type, fields, and listing.
+ * Version:           1.0.0
+ * Requires at least: 6.4
+ * Requires PHP:      8.3
+ * Text Domain:       events
+ *
+ * @package Event_Listing
+ * @since   1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'EVENTS_VERSION', '1.0.0' );
+define( 'EVENTS_FILE', __FILE__ );
+define( 'EVENTS_PATH', plugin_dir_path( __FILE__ ) );
+define( 'EVENTS_URL', plugin_dir_url( __FILE__ ) );
+
+require_once EVENTS_PATH . 'includes/class-post-type.php';
+require_once EVENTS_PATH . 'includes/class-event-fields.php';
+require_once EVENTS_PATH . 'includes/class-plugin.php';
+
+Event_Listing\Plugin::instance();
+
+register_activation_hook( EVENTS_FILE, array( Event_Listing\Plugin::class, 'activate' ) );
+register_deactivation_hook( EVENTS_FILE, array( Event_Listing\Plugin::class, 'deactivate' ) );
